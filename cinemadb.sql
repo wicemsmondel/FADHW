fd6cf9f184ef1622b7a150818429ea3bb3f3ad5f
wicem smondel <w.smondel@gmail.com>
2019-05-21 17:15:25 +0200

    adding data into database

--- cinemadb.sql
+++ cinemadb.sql
@@ -53,7 +53,7 @@ CREATE TABLE `categorie` (
   `id_categorie` int(11) NOT NULL AUTO_INCREMENT,
   `nom_catégorie` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_categorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,6 +62,7 @@ CREATE TABLE `categorie` (
 
 LOCK TABLES `categorie` WRITE;
 /*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
+INSERT INTO `categorie` VALUES (1,'action'),(2,'comedie'),(3,'fantastique'),(4,'horreur'),(5,'animation'),(6,'super-heros');
 /*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,7 +80,7 @@ CREATE TABLE `contact` (
   `email_contact` varchar(45) DEFAULT NULL,
   `message_contact` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_contact`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +89,7 @@ CREATE TABLE `contact` (
 
 LOCK TABLES `contact` WRITE;
 /*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (1,'nom1','prenom1','email1@gmail.com','message1'),(2,'nom2','prenom2','email2@gmail.com','message2'),(3,'nom3','prenom3','email3@gmail.com','message3'),(4,'nom4','prenom4','email4@gmail.com','message4'),(5,'nom5','prenom5','email5@gmail.com','message5');
 /*!40000 ALTER TABLE `contact` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +122,7 @@ CREATE TABLE `film` (
   CONSTRAINT `id_format` FOREIGN KEY (`id_format`) REFERENCES `format` (`id_format`),
   CONSTRAINT `id_statut` FOREIGN KEY (`id_statut`) REFERENCES `statut` (`id_statut`),
   CONSTRAINT `id_version` FOREIGN KEY (`id_version`) REFERENCES `version` (`id_version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,6 +131,7 @@ CREATE TABLE `film` (
 
 LOCK TABLES `film` WRITE;
 /*!40000 ALTER TABLE `film` DISABLE KEYS */;
+INSERT INTO `film` VALUES (24,'Avengers : Endgame','images/avengers.jpg','Mark Ruffalo, Scarlett Johansson, Robert Downey Jr.',182,'Thanos ayant anéanti la moitié de l\'univers, les Avengers restants resserrent les rangs dans ce vingt-deuxième film des Studios Marvel, grande conclusion d\'un des chapitres de l\'Univers Cinématographique Marvel.','https://youtu.be/wV-Q0o2OQjQ','2019-04-24',2,1,1,6),(25,'Shazam!','images/shazam.jpg','Zachary Levi, Asher Angel, Mark Strong',132,'On a tous un super-héros qui sommeille au fond de soi… il faut juste un peu de magie pour le réveiller. Pour Billy Batson, gamin débrouillard de 14 ans placé dans une famille d\'accueil, il suffit de crier \"Shazam !\" ','https://youtu.be/m1eE2BEPua0','2019-04-03',2,1,1,6),(26,'Toy story 4','images/toy_story_4.jpg','Joan Cusack, Tom Hanks, Tony Hale',93,'Le retour de Buzz l\'éclair, Woody et les autres pour une quatrième aventure...','https://youtu.be/KIgbEvK2GOU','2019-06-26',2,2,1,5),(27,'Simetierre','images/simetierre.jpg','Jason Clarke, John Lithgow',111,'Le docteur Louis Creed, sa femme Rachel et leurs deux jeunes enfants quittent Boston pour s\'installer dans une région rurale du Maine. Près de sa maison, le docteur découvre un mystérieux cimetière caché au fond des bois...','https://youtu.be/YYhTeJjFO7c','2019-04-10',1,1,2,4),(28,'La malédiction de la Dame Blanche','images/la_malediction_de_la_dame_blanche.jpg','Linda Cardellini, Raymond Cruz, Tony Amendola',94,'La Dame Blanche. Spectre terrifiant, pris en étau entre le paradis et l\'enfer, piégé par un terrible destin dont elle est elle-même l\'artisan. La seule évocation de son nom sème la terreur dans le monde depuis des siècles. ','https://youtu.be/NlVWRyA8eAc','2019-04-17',1,1,1,4),(29,'Pokemon Detective Pikachu','images/pokemon_detective_pikachu.jpg','Suki Waterhouse, Bill Nighy, Ken Watanabe, ',104,'Après la disparition mystérieuse de Harry Goodman, un détective privé, son fils Tim va tenter de découvrir ce qui s\'est passé.  Le détective Pikachu, ancien partenaire de Harry, participe alors à l\'enquête : un super-détective adorable','https://youtu.be/pAOeW9JixYQ','2019-05-08',2,1,1,2);
 /*!40000 ALTER TABLE `film` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +146,7 @@ CREATE TABLE `format` (
   `id_format` int(11) NOT NULL AUTO_INCREMENT,
   `nom_format` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_format`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,6 +155,7 @@ CREATE TABLE `format` (
 
 LOCK TABLES `format` WRITE;
 /*!40000 ALTER TABLE `format` DISABLE KEYS */;
+INSERT INTO `format` VALUES (1,'2D'),(2,'3D');
 /*!40000 ALTER TABLE `format` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -166,7 +170,7 @@ CREATE TABLE `newsletter` (
   `id_newsletter` int(11) NOT NULL AUTO_INCREMENT,
   `email_newsletter` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_newsletter`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -175,6 +179,7 @@ CREATE TABLE `newsletter` (
 
 LOCK TABLES `newsletter` WRITE;
 /*!40000 ALTER TABLE `newsletter` DISABLE KEYS */;
+INSERT INTO `newsletter` VALUES (1,'newsletter1@gmail.com'),(2,'newsletter2@gmail.com'),(3,'newsletter3@gmail.com'),(4,'newsletter4@gmail.com'),(5,'newsletter5@gmail.com');
 /*!40000 ALTER TABLE `newsletter` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -248,7 +253,7 @@ CREATE TABLE `salle` (
   `nom_salle` varchar(45) NOT NULL,
   `capacite_salle` int(11) NOT NULL,
   PRIMARY KEY (`id_salle`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -257,6 +262,7 @@ CREATE TABLE `salle` (
 
 LOCK TABLES `salle` WRITE;
 /*!40000 ALTER TABLE `salle` DISABLE KEYS */;
+INSERT INTO `salle` VALUES (1,'salle 1',10),(2,'salle 2',20),(3,'salle 3',30),(4,'salle 4',40),(5,'salle 5',50);
 /*!40000 ALTER TABLE `salle` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,7 +308,7 @@ CREATE TABLE `statut` (
   `id_statut` int(11) NOT NULL AUTO_INCREMENT,
   `nom_statut` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_statut`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -311,6 +317,7 @@ CREATE TABLE `statut` (
 
 LOCK TABLES `statut` WRITE;
 /*!40000 ALTER TABLE `statut` DISABLE KEYS */;
+INSERT INTO `statut` VALUES (1,'a l\'affiche'),(2,'prochainement'),(3,'avant-premiere');
 /*!40000 ALTER TABLE `statut` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -325,7 +332,7 @@ CREATE TABLE `version` (
   `id_version` int(11) NOT NULL AUTO_INCREMENT,
   `langue_version` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -334,6 +341,7 @@ CREATE TABLE `version` (
 
 LOCK TABLES `version` WRITE;
 /*!40000 ALTER TABLE `version` DISABLE KEYS */;
+INSERT INTO `version` VALUES (1,'VF'),(2,'VOSTFR');
 /*!40000 ALTER TABLE `version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -346,4 +354,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-15 17:06:06
+-- Dump completed on 2019-05-21 17:11:40
